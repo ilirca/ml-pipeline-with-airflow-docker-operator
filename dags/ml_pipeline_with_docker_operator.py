f2b6d5fa4ece1aa208524826cbfd2d24cc0e0b49
icafaj <ilir.cafaj@vodafoneziggo.com>
2024-12-10 22:02:00 +0100

    IC working copy with local airflow in venv

--- dags/ml_pipeline_with_docker_operator.py
+++ dags/ml_pipeline_with_docker_operator.py
@@ -16,7 +16,7 @@ with DAG(
     description='Run ML pipeline with docker operator in Airflow locally',
     schedule_interval=None,  # Set your desired schedule interval or use None for manual triggering
 ) as dag:
-    
+
     dataset_creation_task = BashOperator(
         task_id="faked_dataset_creation_task",
         bash_command="""
@@ -33,11 +33,12 @@ echo "Hey the dataset is ready, let's trigger the training process"
         container_name="training_my_awesome_model",
         environment={
             'MINIO_ENDPOINT': 'host.docker.internal:9000',
-            'MINIO_ACCESS_KEY_ID': 'nOmjo49pMf08zwZ4',
-            'MINIO_SECRET_ACCESS_KEY': 'DjA2RQh85I6JtDO9Kho2667XNmMNzjfo',
+            'MINIO_ACCESS_KEY_ID': 'w53yvcIN2mW6aA597uwc',
+            'MINIO_SECRET_ACCESS_KEY': 'pgtwlVDdPRLK6Y6MhOtV7XmRXSMQ7NUR5xwcielC',
             'MINIO_BUCKET_NAME': 'coder2j-awesome-ml-artifacts',
         },  # Set environment variables inside the contain
-        command=['python', 'model_tuning.py'],  # Replace with the command you want to run inside the container
+        # command=['python', 'model_tuning.py'],  # Replace with the command you want to run inside the container
+        command=['python', 'train_and_publish.py'],  # test ilir: it does same as in Dockerfile - trains and save pkl file
         # network_mode='bridge',  # Specify the network mode if needed
         # volumes=['/host/path:/container/path'],  # Mount volumes if needed
         dag=dag,
